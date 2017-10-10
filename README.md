67297c2d6349c315b39d3d3167ef99833ce12c07
Adithya Murali <adithyamurali.github@gmail.com>
2017-10-10 00:02:04 -0700

    Stable use of routemap with other middlewares for express

---
+++ README.md
@@ -0,0 +1,15 @@
+# RouteMap Boiler Project
+## Middlewares
+* logging-express-mw -  allow routemap to log and traces functions
+* routemap-express-mw - routemap will allow a user to write elegant apis with a standardized workflow of promise that is logged and easy to debug
+
+## Other dependencies
+* dotenv -  Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env
+* body-parser - Node.js body parsing middleware.
+* cors - CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
+* swagger-express-mw - express middle using swagger routes
+
+## Usage
+* Copy environment variables: ```cp .env.sample .env```
+* Start swagger server: ```swagger project start```
+* Edit api/docs: ```swagger project edit```
---
+++ app.js
@@ -0,0 +1,56 @@
+'use strict';
+// Middleware for contextual logging
+var logger = require('logging-express-mw');
+// Middleware for writing good apis
+var routeMap = require('routemap-express-mw');
+// Parse incoming request bodies in a middleware
+// before your handlers, available under
+// the req.body property.
+var bodyParser = require('body-parser');
+// Middleware that enables CORS requests
+var cors = require('cors');
+// Load environment variables
+require('dotenv').config()
+
+var SwaggerExpress = require('swagger-express-mw');
+var app = require('express')();
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname // required config
+};
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+// CORS enabled with options
+// {
+//   "origin": "*",
+//   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   "preflightContinue": false,
+//   "optionsSuccessStatus": 204
+// }
+app.use(cors());
+
+// Need to use `req.logger`
+app.use(logger.middleware());
+// mw to write elegant apis
+app.use(routeMap());
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
+
+  // install middleware
+  swaggerExpress.register(app);
+
+  var port = process.env.PORT || 10010;
+  app.listen(port);
+
+  if (swaggerExpress.runner.swagger.paths['/hello']) {
+    console.log(`try this:\ncurl http://127.0.0.1:' ${port} /hello?name=routemap`);
+  }
+});
